edf0a3dcd35797cb60688ceaf993960dce32b54c
Devichand1 <devichandinikhiya@gmail.com>
2021-07-13 16:19:17 +0530

    fullname

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -2,7 +2,7 @@ const { check, validationResult } =require('express-validator');
 
 exports.Validatesignuprequest =
     [
-        check('fullname').notEmpty().withMessage("Please Enter Your firstname") ,
+        check('fullname').notEmpty().withMessage("Please Enter Your fullname") ,
         check('email').isEmail().withMessage('Valid Email is required'),
         check('password').isLength({min:6}).withMessage('Password must be at least 6 charactors long'),
      ];
